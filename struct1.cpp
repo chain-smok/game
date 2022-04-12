2da34a538bcca5a6ba26f5cb6d677e3d8118a3c7
chain-smok <101109105+chain-smok@users.noreply.github.com>
2022-04-12 15:52:16 +0800

    updated response object

--- struct1.cpp
+++ struct1.cpp
@@ -1,13 +1,19 @@
 #include <bits/stdc++.h>
-struct stdRec{
+struct stdRec/*type*/{
+  char name[20];
   int id;
- char name[20];
+  int total;
 };
 int main(){
-   stdRec a={1,"mary"};
-   stdRec b={2,"John"};
-   printf("a.id=%d,name=%s\n",a.id,a.name);
-   printf("b.id=%d,name=%s\n",b.id,b.name);
+   stdRec a/*variable*/={"mary",1,100}, b={"john"},*c;//{} is assignment operator
+   c=(struct stdRec*)malloc(sizeof(struct stdRec));
+   c->id=1;
+   (*c).total=100;
+   strcpy(c->name,"mary");
+   int x=sizeof(struct stdRec);
+   printf("c.id=%d,name=%s and total=%d\n",c->id,c->name,c->total);
+   printf("a.id=%d,name=%s and total=%d\n",a.id,a.name,a.total);
+   printf("b.id=%d,name=%s and total=%d\n",b.id,b.name,b.total);
    a=b;
     printf("a.id=%d,name=%s\n",a.id,a.name);
    /*if(a==b){
